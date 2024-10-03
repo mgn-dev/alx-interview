936eda2e64afbd5e58ba522edeaab94677cf6e45
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-10-04 00:43:21 +0200

    Do task 0, project 0x00. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
 
     for i in range(n):
         row = [1] * (i + 1)  # Each row starts with 1s
-        if i > 1:  # Update the internal values for rows greater than 2
+        if i > 1:  # Update internal values for rows greater than 2
             for j in range(1, i):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
