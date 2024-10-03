107399c83c8b54e4a3fa94574e51b28711991ae3
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-10-04 00:40:07 +0200

    Do task 3, project 0x01. ES6 Promises

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module to generate Pascal's triangle.
+"""
+
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to n rows."""
+    if n <= 0:
+        return []
+
+    triangle = []
+    
+    for i in range(n):
+        row = [1] * (i + 1)  # Each row starts with 1s
+        if i > 1:  # Update the internal values for rows greater than 2
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
